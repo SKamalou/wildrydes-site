95f3b68cd89b5464ba69e9c02cfdbca86f5e7aaf
Sadegh Kamalou <mr.kamalou@gmail.com>
2025-08-05 17:44:54 +0200

    Update config.js
    d

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_uPJ8aTyzV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5n24ionou1br022fdblico5vnt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5og0h9sch1hijkfinbitv2cb33', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
